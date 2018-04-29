336f42fb7b7817834146f7e01cfdf9e6ed63e0b9
Ron Lapushner <lapushnerron@gmail.com>
2018-04-29 14:18:29 -0500

    show follow/unfollow button as needed

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -12,32 +12,53 @@ import { DbService } from '../db.service'
 export class SearchComponent implements OnInit {
 
   myUid: string
-  
-  followList: Observable<any>
+
+  followList: Array<any>
 
   users: Observable<any>
 
-  constructor(private db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+  constructor(
+    private db: AngularFireDatabase,
+    public afAuth: AngularFireAuth,
+    private dbs: DbService,
+    private cdr: ChangeDetectorRef
+  ) {
     this.db = db
     this.myUid = afAuth.auth.currentUser.uid
-    this.followList = db.list('follow/' + this.myUid).valueChanges();
     this.users = db.list('users').valueChanges();
+    this.followList = []
+    this.dbs.myFollows.subscribe((follow) => {
+      this.followList.push(follow)
+      cdr.detectChanges()
+    })
   }
 
-  followUser(uid){
-    var follow = {}
-    follow[this.myUid] = uid
-    this.db.list(`follow/${this.myUid}`).set(uid, {timestamp: String(Date.now()).slice(0,9)} )
-    this.db.list(`followers/${uid}`).set(this.myUid, {timestamp: String(Date.now()).slice(0,9)} )    
+  isInFollowList(uid) {
+    let isInList = false
+
+    this.followList.forEach(list => {
+      isInList = list.some(follow =>
+        follow['uid'] === uid
+      )
+    })
+
+    return isInList
   }
 
-  unfollowUser(uid){
+  followUser(uid) {
     var follow = {}
     follow[this.myUid] = uid
-    this.db.list(`follow/${this.myUid}`).remove( uid )
+    this.db.list(`follow/${this.myUid}`).set(uid, { timestamp: String(Date.now()).slice(0, 9) })
+    this.db.list(`followers/${uid}`).set(this.myUid, { timestamp: String(Date.now()).slice(0, 9) })
+  }
+
+  unfollowUser(uid) {
+    var follow = {}
+    follow[this.myUid] = uid
+    this.db.list(`follow/${this.myUid}`).remove(uid)
     this.db.list(`followers/${uid}/${this.myUid}`).remove()
   }
-  
+
   ngOnInit() {
   }
 
